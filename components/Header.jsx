d12f3cd08736193cc81d72d9f82bf0e538f4afaa
TARO <shun.ichiro.tanaka16@gmail.com>
2022-03-20 20:32:06 +0900

    スタイル修正

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,20 +12,15 @@ function Header() {
   }, []);
 
   return (
-    <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-blue-400 py-8">
-        <div className="md:float-left block">
+    <header className="mx-auto mb-8 bg-white">
+      <div className="w-full inline-block py-6">
+        <div className="px-10">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">Graph CMS</span>
+            <span className="title cursor-pointer font-bold text-2xl">Frontend Diary</span>
           </Link>
         </div>
-        <div className="hidden md:float-left md:contents">
-          {categories.map((category, index) => (
-            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
-          ))}
-        </div>
       </div>
-    </div>
+    </header>
   );
 }
 
--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -48,7 +48,7 @@ function PostCard({ post }) {
       </p>
       <div className="text-center">
         <Link href={`/post/${post.slug}`}>
-          <span className="transition duration-500 ease transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">Continue Reading</span>
+          <span className="btn transition duration-300 ease transform hover:-translate-y-1 inline-block text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">Continue Reading</span>
         </Link>
       </div>
     </div>
